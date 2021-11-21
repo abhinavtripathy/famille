778b9214b55a62ccdbb99b9a6cc32706fba36102
abhinavtripathy <abhinavtripathy99@gmail.com>
2021-11-21 12:56:11 -0500

    adding vue for family members

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 // import LoadScript from 'vue-plugin-load-script';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 // Vue.use(LoadScript)
--- client/src/views/AddFamily.vue
+++ client/src/views/AddFamily.vue
@@ -1,9 +1,68 @@
 <template>
   <div>
- <h1>Hello world</h1>
+      <h2>Add Your Family Members!</h2>
+      <b-container class="bv-example-row">
+<div v-for="index in 10" :key="index">
+  <b-row>
+      <b-col>{{ index }}.</b-col>
+    <b-col><b-form-input
+      id="inline-form-input-name"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Name"
+      v-model="form[index]['name']"
+    ></b-form-input></b-col>
+    <b-col><b-form-input
+      id="inline-form-input-name"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Nickname"
+      v-model="form[index]['nickname']"
+    ></b-form-input></b-col>
+    <b-col><b-form-input
+      id="inline-form-input-name"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Image Link"
+      v-model="form[index]['img']"
+    ></b-form-input></b-col>
+    <b-col><b-form-input
+      id="inline-form-input-name"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Partner ID"
+      v-model="form[index]['partner']"
+    ></b-form-input></b-col>
+    <b-col><b-form-input
+      id="inline-form-input-name"
+      class="mb-2 mr-sm-2 mb-sm-0"
+      placeholder="Parents IDs"
+      v-model="form[index]['parent']"
+    ></b-form-input></b-col>
+  </b-row>
+  <br>
+ </div>
+</b-container>
+<br>
+ <b-button v-b-modal.modal-1 variant="primary" @click="submitForm">Submit!</b-button>
+ <b-modal id="modal-1" title="Add Family">
+    <p class="my-4">Success! You have successfully added your family!</p>
+  </b-modal>
   </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            form: {}
+        }
+    },
+    created() {
+        for(let i = 1; i <= 10; i++) {
+            this.form[i] = {}
+        }
+    },
+    methods: {
+        submitForm () {
+            console.log(this.form)
+        }
+    }
+}
 </script>
